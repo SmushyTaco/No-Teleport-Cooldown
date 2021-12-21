499de613439a33b999d6c53b1018f6430f491f0d
SmushyTaco <smushytaco@gmail.com>
2021-12-21 01:33:33 -0800

    1.18 Update

--- No Teleport Cooldown/src/main/kotlin/com/smushytaco/no_teleport_cooldown/configuration_support/ModMenuIntegration.kt
+++ No Teleport Cooldown/src/main/kotlin/com/smushytaco/no_teleport_cooldown/configuration_support/ModMenuIntegration.kt
@@ -1,14 +1,11 @@
 package com.smushytaco.no_teleport_cooldown.configuration_support
-import com.smushytaco.no_teleport_cooldown.NoTeleportCooldown
-import io.github.prospector.modmenu.api.ConfigScreenFactory
-import io.github.prospector.modmenu.api.ModMenuApi
+import com.terraformersmc.modmenu.api.ConfigScreenFactory
+import com.terraformersmc.modmenu.api.ModMenuApi
 import me.shedaniel.autoconfig.AutoConfig
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 @Environment(EnvType.CLIENT)
 class ModMenuIntegration: ModMenuApi {
-    override fun getModId() = NoTeleportCooldown.MOD_ID
-    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> {
-        return ConfigScreenFactory { parent -> AutoConfig.getConfigScreen(ModConfiguration::class.java, parent).get() }
-    }
+    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> =
+        ConfigScreenFactory { parent -> AutoConfig.getConfigScreen(ModConfiguration::class.java, parent).get() }
 }
