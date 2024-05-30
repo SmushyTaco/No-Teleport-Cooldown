c762be98119b58e54cef13544384cabf85b4cd1c
Nikan Radan <personal@nikanradan.com>
2024-05-29 23:13:25 -0700

    1.20.6

--- No Teleport Cooldown/src/main/kotlin/com/smushytaco/no_teleport_cooldown/NoTeleportCooldownPreLaunch.kt
+++
@@ -1,5 +0,0 @@
-package com.smushytaco.no_teleport_cooldown
-import com.llamalad7.mixinextras.MixinExtrasBootstrap
-import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint
-@Suppress("UNUSED")
-object NoTeleportCooldownPreLaunch: PreLaunchEntrypoint { override fun onPreLaunch() { MixinExtrasBootstrap.init() } }
