d4d77dd9e84d7b856c3697d1f545dc650bc5da23
Nikan Radan <github.com@alias.nikanradan.com>
2025-11-28 20:05:00 -0800

    Official Mappings

--- No Teleport Cooldown/src/main/java/com/smushytaco/no_teleport_cooldown/mixin/RemoveTeleportCooldown.java
+++ No Teleport Cooldown/src/main/java/com/smushytaco/no_teleport_cooldown/mixin/RemoveTeleportCooldown.java
@@ -3,20 +3,20 @@ import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.smushytaco.no_teleport_cooldown.NoTeleportCooldown;
-import net.minecraft.entity.player.ItemCooldownManager;
-import net.minecraft.item.Items;
-import net.minecraft.registry.Registries;
-import net.minecraft.util.Identifier;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemCooldowns;
+import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import java.util.Map;
-@Mixin(ItemCooldownManager.class)
+@Mixin(ItemCooldowns.class)
 public abstract class RemoveTeleportCooldown {
-    @WrapOperation(method = "set(Lnet/minecraft/util/Identifier;I)V", at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"))
-    private <K, V> V hookSetPut(Map<K, V> instance, K key, V value, Operation<V> original, Identifier groupId, int duration) {
-        if ((groupId != Registries.ITEM.getId(Items.ENDER_PEARL) || !NoTeleportCooldown.INSTANCE.getConfig().isEnderPearlCooldownDisabled()) && (groupId != Registries.ITEM.getId(Items.CHORUS_FRUIT) || !NoTeleportCooldown.INSTANCE.getConfig().isChorusFruitCooldownDisabled())) return original.call(instance, key, value);
+    @WrapOperation(method = "addCooldown(Lnet/minecraft/resources/ResourceLocation;I)V", at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"))
+    private <K, V> V hookSetPut(Map<K, V> instance, K key, V value, Operation<V> original, ResourceLocation groupId, int duration) {
+        if ((groupId != BuiltInRegistries.ITEM.getKey(Items.ENDER_PEARL) || !NoTeleportCooldown.INSTANCE.getConfig().isEnderPearlCooldownDisabled()) && (groupId != BuiltInRegistries.ITEM.getKey(Items.CHORUS_FRUIT) || !NoTeleportCooldown.INSTANCE.getConfig().isChorusFruitCooldownDisabled())) return original.call(instance, key, value);
         return null;
     }
-    @WrapWithCondition(method = "set(Lnet/minecraft/util/Identifier;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/ItemCooldownManager;onCooldownUpdate(Lnet/minecraft/util/Identifier;I)V"))
-    private boolean hookSetOnCooldownUpdate(ItemCooldownManager instance, Identifier groupId, int duration, Identifier groupIdTwo, int durationTwo) { return (groupIdTwo != Registries.ITEM.getId(Items.ENDER_PEARL) || !NoTeleportCooldown.INSTANCE.getConfig().isEnderPearlCooldownDisabled()) && (groupIdTwo != Registries.ITEM.getId(Items.CHORUS_FRUIT) || !NoTeleportCooldown.INSTANCE.getConfig().isChorusFruitCooldownDisabled()); }
+    @WrapWithCondition(method = "addCooldown(Lnet/minecraft/resources/ResourceLocation;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemCooldowns;onCooldownStarted(Lnet/minecraft/resources/ResourceLocation;I)V"))
+    private boolean hookSetOnCooldownUpdate(ItemCooldowns instance, ResourceLocation groupId, int duration, ResourceLocation groupIdTwo, int durationTwo) { return (groupIdTwo != BuiltInRegistries.ITEM.getKey(Items.ENDER_PEARL) || !NoTeleportCooldown.INSTANCE.getConfig().isEnderPearlCooldownDisabled()) && (groupIdTwo != BuiltInRegistries.ITEM.getKey(Items.CHORUS_FRUIT) || !NoTeleportCooldown.INSTANCE.getConfig().isChorusFruitCooldownDisabled()); }
 }
